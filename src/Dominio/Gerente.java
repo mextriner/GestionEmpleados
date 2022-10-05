9a0eea6b0f7494d86c0264ef889aaa955f2592df
mextriner <mextrinerx1998@gmail.com>
2022-10-05 13:37:41 +0200

    Update Gerente.java

--- src/Dominio/Gerente.java
+++ src/Dominio/Gerente.java
@@ -9,7 +9,31 @@ package Dominio;
  *
  * @author Alumno Mañana
  */
-public class Gerente {
-    double salario;
+public class Gerente extends Empleado{
+    String departamento;
     String estudios;
+
+    public Gerente(String departamento, String estudios, String numSegSocial, float salario, String nombre, String apellido) {
+        super(numSegSocial, salario, nombre, apellido);
+        this.departamento = departamento;
+        this.estudios = estudios;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getEstudios() {
+        return estudios;
+    }
+
+    public void setEstudios(String estudios) {
+        this.estudios = estudios;
+    }
+    
+    
 }
